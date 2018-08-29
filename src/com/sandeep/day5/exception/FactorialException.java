85d867217b3c3035ac3c15f77bf67c64d437cc09
sandeeda <sandeeda@IN-MUM-LDVM024.corp.capgemini.com>
2018-08-29 08:56:13 -0400

    day 5 final commit

---
+++ src/com/sandeep/day5/exception/FactorialException.java
@@ -0,0 +1,11 @@
+package com.sandeep.day5.exception;
+
+public class FactorialException extends Exception {
+
+
+	public FactorialException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
